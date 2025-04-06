3bac86ce6cf9efa137934b43f4e91e3a3cec017f
Josvme <josvme@protonmail.com>
2025-04-06 11:57:24 +0200

    First commit with cuda

---
+++ README.md
@@ -0,0 +1,2 @@
+# Source
+https://github.com/clementpoiret/nix-python-devenv/tree/cuda
---
+++ devenv.nix
@@ -0,0 +1,41 @@
+{ pkgs, lib, config, inputs, ... }:
+let
+  buildInputs = with pkgs; [
+    cudaPackages.cuda_cudart
+    cudaPackages.cudatoolkit
+    cudaPackages.cudnn
+    stdenv.cc.cc
+    libuv
+    zlib
+  ];
+in 
+{
+  packages = with pkgs; [
+    cudaPackages.cuda_nvcc
+  ];
+
+  env = {
+    LD_LIBRARY_PATH = "${
+      with pkgs;
+      lib.makeLibraryPath buildInputs
+    }:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+    XLA_FLAGS = "--xla_gpu_cuda_data_dir=${pkgs.cudaPackages.cudatoolkit}"; # For tensorflow with GPU support
+    CUDA_PATH = pkgs.cudaPackages.cudatoolkit;
+  };
+
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  scripts.hello.exec = "uv run python hello.py";
+
+  enterShell = ''
+    . .devenv/state/venv/bin/activate
+    nvcc -V
+    hello
+  '';
+}
